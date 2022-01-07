9feaac894f55a533328bc36c31bb79cd41a72447
jackosnernst <jaxernst@gmail.com>
2022-01-07 13:49:46 -0800

    Added json loader

--- util/obj_funcs.py
+++ util/obj_funcs.py
@@ -1,4 +1,6 @@
 import pickle
+import json
+
 def save_obj(obj, name):
     with open('obj/'+ name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
@@ -12,3 +14,8 @@ def save_pairs(PairsDict):
     for key, val in PairsDict.items():
         out[key] = (val.bid, val.ask)
     save_obj(out, "pairs")
+
+def load_json(filename):
+    with open(filename) as f:
+        dict_ = json.load(f)
+    return dict_
