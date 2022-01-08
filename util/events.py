aadbc4e9ee6f5ef91b4b9625b9b03c30981e776e
jackosnernst <jaxernst@gmail.com>
2022-01-08 13:08:57 -0800

    Handle multiple subscribtions to the same event

--- util/events.py
+++ util/events.py
@@ -2,9 +2,12 @@ subscribers = {}
 
 def subscribe(event_type: str, fn):
     if event_type not in subscribers:
-        subscribers[event_type] = fn
+        subscribers[event_type] = [fn]
+    else:
+        subscribers[event_type].append(fn)
 
 def post_event(event_type: str, data):
     if not event_type in subscribers:
         return
-    subscribers[event_type](data)
+    for fn in subscribers[event_type]:
+        fn(data)
