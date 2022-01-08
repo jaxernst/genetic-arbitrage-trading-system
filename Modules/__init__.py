e6334a179270ff7cdc96cb912195ee6f76669a77
jackosnernst <jaxernst@gmail.com>
2022-01-08 13:17:31 -0800

    Merged Session classes into single file

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -1,7 +1,7 @@
 from Modules.DataManagement import ExchangeData, Pair
 from Modules.GeneticArbitrage import GeneticArbitrage
 from Modules.TradeExecution import TradeExecution
-from Modules.SessionLive import SessionLive
-from Modules.SessionSim import SessionSim
+from Modules.Sessions import SessionLive, SessionSim
+
 
 
