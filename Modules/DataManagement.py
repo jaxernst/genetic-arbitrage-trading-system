ec2648e0e03405bd13d576d98b3baff2cce41122
jackosnernst <jaxernst@gmail.com>
2022-01-12 08:15:26 -0800

    Support level2 orderbook stream

--- Modules/DataManagement.py
+++ Modules/DataManagement.py
@@ -1,19 +1,59 @@
+import dataclasses
 from typing import Tuple, Dict
 from dataclasses import dataclass
 from APIs.abstract import ExchangeAPI
 from util.currency_filters import remove_single_swapabble_coins
 from util import events
+from util.obj_funcs import save_json
+import bisect
 import time
 
+@dataclass
+class Orderbook:
+    last_sequence: int = None
+    bids: list = None
+    asks: list = None
+    missing_sequences:list=None
+
+    def update(self, type, price, size, sequence:int):
+        if int(sequence) != self.last_sequence + 1:
+            if not self.missing_sequences:
+                self.missing_sequences = []
+            self.missing_sequences.append(sequence)     
+        self.last_sequence = sequence
+        
+        if price == "0":
+            return
+        if type == "bids":
+            self.bids[price] = size
+            if size == "0": 
+                self.bids.pop(price)
+        elif type == "asks":
+            self.asks[price] = size
+            if size == "0": 
+                self.asks.pop(price)
+        
+    def get_book(self, type):
+        out = []
+        if type == 'bids':
+            book_sorted = sorted(self.bids.items(), reverse=True)
+        elif type == 'asks':
+            book_sorted = sorted(self.asks.items())
+        else:
+            raise Exception("Invalid type)")
+
+        for price, size in book_sorted:
+            out.append([float(price), float(size)])
+        return out
 
 @dataclass
 class Pair:
     base: str
     qoute: str
-    bid: float = None
-    ask: float = None
+    best_bid: float = None
+    best_ask: float = None
     close: float = None
-    orderbook: dict = None
+    orderbook: Orderbook = None
     lastUpdated: float = time.time()
     baseIncrement: float = None
     qouteIncrement: float = None
@@ -24,9 +64,10 @@ class Pair:
         self.ticker = f"{self.base}/{self.qoute}"
         self.sym = self.base + "/" + self.qoute
 
-    def spread_populated(self):
-        return bool(self.bid and self.ask)
+    def fee_spread_populated(self):
+        return isinstance(self.orderbook.bids,dict) and isinstance(self.orderbook.asks,dict) and self.fee
 
+   
 class ExchangeData:
     PAIR_UPDATE_EVENT_ID = "ExchangeDataPairUpdate"
     
@@ -34,15 +75,77 @@ class ExchangeData:
         self.API = API
         self.Pairs = {} # List of 
         self.Orders = {}
+        self.balanceUpdates = []
         self.skipCurrencies = [] # Currencies to not collect data on
         self.showPairUpdates = False
-        
+        self.num_missing_fees = None
+        self.missing_fees  = []
+        self.level2_calibrated = False
+        self.orderbook_updates = 0
+
         events.subscribe(API.PAIR_UPDATE_EVENT_ID, self.pair_update_listener)
-        #events.subscribe(API.ORDER_UPDATE_EVENT_ID, self.order_update_listener)
+        events.subscribe(API.ORDER_UPDATE_EVENT_ID, self.order_update_listener)
+        events.subscribe(API.ACCOUNT_BALANCE_UPDATE_EVENT_ID, self.account_balance_update_listener)
+        events.subscribe(API.LEVEL2_UPDATE_EVENT_ID, self.level_2_update_listener)
+        events.subscribe(API.DISCONNECT_EVENT_ID, self.build_orderbook)
+
+    def build_orderbook(self):
+        '''Creates orderbook for each pair, start oderbook update stream, and calibrates the ordebook with the sequencial updates'''
+        self.level2_calibrated = False
+        self.orderbook_cache = {}
+        print("Subscribing to ordebook stream...")
+        self.API.subscribe_level2(list(self.Pairs.keys()))
+        time.sleep(.4) # Delay to allow orders to get cached
+        print("Getting orderbook snapshot...")
+        snapshot = self.API.get_multiple_orderbooks(list(self.Pairs.keys()))
         
+        # Create ordebook objects for each pair
+        for pair in snapshot:
+            asks = {price:size for price, size in snapshot[pair]["asks"]}
+            bids = {price:size for price, size in snapshot[pair]["bids"]}
+            self.Pairs[pair].orderbook.asks = asks
+            self.Pairs[pair].orderbook.bids = bids
+            self.Pairs[pair].orderbook.last_sequence = int(snapshot[pair]['sequence'])
+        
+        # Playback cache
+        print(f"Initial orderbook built, calibrating with {len(self.orderbook_cache)} cached orderbook increments...")
+        for pair in list(self.orderbook_cache):
+            for cache_sequence in list(self.orderbook_cache[pair]):
+                # Discard cache_sequenced orderbook change data from sequences that came before the ordebook was built
+                if int(cache_sequence) > self.Pairs[pair].orderbook.last_sequence:
+                    type, price, size = self.orderbook_cache[pair][cache_sequence]
+                    self.Pairs[pair].orderbook.update(type, price, size, int(cache_sequence))
+            try:
+                print(f"{pair} calibration status set to True with {len(self.Pairs[pair].orderbook.missing_sequences)} missing sequences")
+            except:
+                print(f"{pair} calibration status set to True with 0 missing sequences")
+
+        self.level2_calibrated = True
+
+    def level_2_update_listener(self, message):
+        base, qoute = message["symbol"].split('-')
+        
+        if not self.level2_calibrated:
+            for type, changes in message['changes'].items():
+                for change in changes:
+                    price, size, sequence_num  = change
+                    if (base,qoute) not in self.orderbook_cache:
+                        self.orderbook_cache[(base,qoute)] = {}
+                    self.orderbook_cache[(base,qoute)][sequence_num] = (type, price, size)
+                    return
+        else:
+            for type, change in message['changes'].items():
+                if change:
+                    price, size, sequence = change[0]
+        
+                    self.Pairs[(base,qoute)].orderbook.update(type, price, size, int(sequence))
+                    self.orderbook_updates += 1
+            a = 1
+
+    
     def pair_update_listener(self, message: Tuple[tuple,Dict[str,str]]) -> None:
         '''
-        This function is called whenpair data is received through the websocket.
+        This function is called when pair data is received through the websocket.
         Mesaage: tuple = (("base","qoute"), {'close':..., 'ask':..., 'bid':...})
         '''
 
@@ -55,6 +158,7 @@ class ExchangeData:
         base = pair[0]
         qoute = pair[1]
         if pair not in self.Pairs:
+            #print(f"Pair {pair} not in pairs.")
             self.Pairs[pair] = Pair(base, qoute)
         
         self.Pairs[pair].close =  float(data['close'])
@@ -66,35 +170,58 @@ class ExchangeData:
             self.show_coins()
 
     def order_update_listener(self, message):
-        print("ExchangeData received order update")
         base, qoute = message['symbol'].split("-")
-        self.Orders[(base,qoute)] = {}
+        
+        if (base, qoute) not in self.Orders:
+            self.Orders[(base,qoute)] = {}
 
-        self.Orders[(base,qoute)]['type'] = message['type']
-        self.Orders[(base,qoute)]['status'] = message['status']
-        self.Orders[(base,qoute)]['filledSize'] = message['filledSize']
+        self.Orders[(base,qoute)] = message
+        
+        #self.Orders[(base,qoute)]['type'] = message['type']
+        #self.Orders[(base,qoute)]['status'] = message['status']
+        #self.Orders[(base,qoute)]['filledSize'] = message['filledSize']
 
+    def account_balance_update_listener(self, message):
+        self.balanceUpdates.append(message)
+
+    def save_orders(self):
+        now = str(time.time()) 
+        save_json(self.Orders, f"orders_{now}")
+        save_json(self.balanceUpdates, f"balances_{now}")
+    
     def show_coins(self):
         for pair in self.Pairs:
             if self.Pairs[pair].close != None:
                 print(self.Pairs[pair])
 
     def make_pairs(self, pairInfo, populateSpread=False):
-        
         for pair, pair_data in pairInfo.items():
             base, qoute = pair
             if (base, qoute) not in self.Pairs:
                 if populateSpread:
                     bid, ask, close = self.update_spread((base,qoute))
-                    self.Pairs[(base, qoute)] = Pair(base, qoute, bid, ask, close, 
+                    self.Pairs[(base, qoute)] = Pair(base, qoute, best_bid=bid, best_ask=ask, close=close, 
+                                                    orderbook=Orderbook(),
                                                     baseIncrement=pair_data['baseIncrement'],
                                                     qouteIncrement=pair_data['qouteIncrement'],
                                                     fee=float(pair_data['fee']))
                 else:
-                    self.Pairs[(base, qoute)] = Pair(base, qoute, 
+                    self.Pairs[(base, qoute)] = Pair(base=base, qoute=qoute, 
+                                                    orderbook=Orderbook(),
                                                     baseIncrement=pair_data['baseIncrement'], 
                                                     qouteIncrement=pair_data['quoteIncrement'],
                                                     fee=float(pair_data['fee']))
+    
+    def update_missing_fee_pairs(self):
+        self.missing_fees  = []
+        for pair_name, pair  in self.Pairs.items():
+            if not pair.fee:
+                self.missing_fees.append(pair_name)
+
+        print(f"Out of {len(self.Pairs)}, {len(self.missing_fees)} are currently missing, updating those fees")
+
+        for pair, fee in self.API.get_pair_fees(self.missing_fees).items():
+            self.Pairs[pair].fee = float(fee)
 
     def update_spread(self, pair):
         return self.API.get_pair_spread(pair)
@@ -108,5 +235,3 @@ class ExchangeData:
                 self.Pairs[pair].close = close
                 self.Pairs[pair].last_updated = time.time()
     
-    def getPriceHistory():
-        pass  
