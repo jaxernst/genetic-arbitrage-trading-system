a5def12172717081355f8255b46fd55b666de812
jackosnernst <jaxernst@gmail.com>
2022-01-18 16:28:19 -0800

    Configured for triangular arb

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+from Modules import DataManagement, GeneticArbitrage, TradeExecution, ExchangeData, SessionSim, SessionLive, TriangularArbitrageEngine
 from APIs import KrakenAPI, KucoinAPI
 from CustomExceptions import OrderVolumeDepthError
-from Modules import GeneticArbitrage, TradeExecution, ExchangeData, SessionSim, SessionLive
 from Modules.Portfolio import Portfolio
 from util.obj_funcs import load_obj, save_obj
-from util.currency_filters import remove_single_swapabble_coins
+from util.currency_funcs import remove_single_swapable_coins, get_usd_value
 from util.SequenceTracker import SequenceTracker
+
 import logging
+from threading import Thread
 import time
 
-SIMULATION_MODE = False
 
+
+
+SIMULATION_MODE = False
 
 # Init API and Data Manager
 KucoinAPI = KucoinAPI()
@@ -17,17 +21,18 @@ ExchangeData = ExchangeData(KucoinAPI)
 
 # Setup pairs for the GA to look at
 pairsInfo = KucoinAPI.get_pair_info()
-viablePairs = remove_single_swapabble_coins(list(pairsInfo.keys()))[:299]
+viablePairs = remove_single_swapable_coins(list(pairsInfo.keys()))[:299]
 pairsInfo = {pair:info for pair, info in pairsInfo.items() if pair in viablePairs}
 ExchangeData.make_pairs(pairsInfo, populateSpread=False)
 ExchangeData.build_orderbook()
 KucoinAPI.maintain_connection()
+time.sleep(10)
    
 # Setup account
-funding_cur = "USDT"
-
+funding_cur = "ETH"
 min_volume = 100
 Account = KucoinAPI.get_portfolio()
+starting_bal = get_usd_value(funding_cur, Account.balance[funding_cur])
 
 if SIMULATION_MODE:
     Session = SessionSim(Account, KucoinAPI, Account.balance[funding_cur], funding_cur, 5) # The session will update the parent account 
@@ -35,60 +40,32 @@ else:
     Session = SessionLive(Account, KucoinAPI, ExchangeData, Account.balance[funding_cur], funding_cur, 5) # The session will update the parent account 
 
 # Setup trade execution Modules
-Trader = TradeExecution(KucoinAPI, ExchangeData, Session, simulation_mode=SIMULATION_MODE)
+Trader = TradeExecution(KucoinAPI, ExchangeData, Session, starting_cur=funding_cur, simulation_mode=SIMULATION_MODE)
+
 
 # Setup arbitrage model
-sequence_length = 3
 set_size = 500
-GA1 = GeneticArbitrage(sequence_length, set_size, ExchangeData, Trader, base_cur=funding_cur)
-
-# Setup sequence tracker (remember sequences)
-# Tracker = SequenceTracker(5)
+TA = TriangularArbitrageEngine(Trader)
 
 
-seq_name = ""
-last_found = None
-GAprofits = []
-sequence_lengths = []
-RealProfits = [-100]
-winners = load_obj("profitable_alts")
-t1 = time.time()
-i = 0
-trades = 0
+
+def display_stats(t1):
+    amount = Session.balance[Trader.owned]
+    usd_amount = get_usd_value(Trader.owned, amount)
+    print("")
+    print(f"Starting balance: {round(starting_bal,3)}")
+    print(f"Percentage Gain: {round(100*(usd_amount - starting_bal)/starting_bal, 3)} %")
+    print(f"Session owned: {Trader.owned}")
+    print(f"Session balance: {round(amount,3)}")
+    print(f"Number of trades: {Session.trades}")
+    print(f"Best TA profit: {round(100*Trader.best_exp_profit,5)} %")
+    print(ExchangeData.orderbook_updates)
+    print(f"Elasped: {round((time.time() - t1)/60, 3)} minutes")
+    print("")
 
 if __name__ == "__main__":
+    TA.begin(start_cur=funding_cur)
+    start_time = time.time()
     while True:
-        GAprofit, sequence = GA1.do_evolution()
-        #print(GAprofit)
-        if GAprofit:
-            GAprofits.append(GAprofit)
-            #profit = Trader.execute_sequence(sequence, Session)
-            if GAprofit > 0:
-                #print("GAprofit found")
-
-                print(f"Potential trade found -- Profit: {round(GAprofit*100, 6)} % || Sequence: {sequence}")
-
-                try:
-                    pass
-                    #profit = Trader.execute_sequence(sequence, Session)
-                except OrderVolumeDepthError:
-                    continue
-
-
-            i += 1
-            if i % 50 == 0:
-                print("")
-                print(f"Session balance: {round(Session.balance['USDT'],3)}")
-                print(f"Percentage Gain: {round(100*Session.PL, 3)} %")
-                print(f"Account balance: {round(Account.balance['USDT'],3)}")
-                print(f"Number of trades: {Session.trades}")
-                print(f"Best GA profit: {round(100*max(GAprofits),5)} %")
-                print(f"Best Real profit: {round(100*max(RealProfits),5)} %")
-                print(ExchangeData.orderbook_updates)
-                if sequence_lengths:
-                    print(f"Average profitable sequence length: {sum(sequence_lengths) / len(sequence_lengths)}")
-                print(f"Elasped: {round((time.time() - t1)/60, 3)} minutes")
-                print("")
-
-        time.sleep(.05)
-
+        display_stats(start_time)
+        time.sleep(10)
