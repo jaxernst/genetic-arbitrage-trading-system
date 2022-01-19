975cc8eab33bebd8562a94e04054dcd68cd92fcc
jackosnernst <jaxernst@gmail.com>
2022-01-18 16:03:10 -0800

    Add currencies to ignore

--- Config.py
+++ Config.py
@@ -1,2 +1,2 @@
-skipCurrencies = [] # Currencies that should not be traded
+skipCurrencies = ["DRGN", "SENSO", "MHC", "AMPL", "BEPRO", "MTV"] # Currencies that should not be traded
 PROFIT_THRESH = .0005 # Minimum expected profit threshold required to trade
