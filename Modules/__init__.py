7cba19ebad4d4c4d0b62144fdf3bb439f85c592c
jackosnernst <jaxernst@gmail.com>
2022-01-18 16:30:19 -0800

    Initialize TriArbEngine

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -1,5 +1,6 @@
 from Modules.DataManagement import ExchangeData, Pair
 from Modules.GeneticArbitrage import GeneticArbitrage
+from Modules.TriangularArbitrage import TriangularArbitrageEngine
 from Modules.TradeExecution import TradeExecution
 from Modules.Sessions import SessionLive, SessionSim
 
